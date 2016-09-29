50dca703b878b089615f0ab32e51584b5f1010d7
josemrobles <joserobles@robi.broward.edu>
2016-09-28 20:24:29 -0400

    able to publish message - WIP

--- main.go
+++ main.go
@@ -5,18 +5,28 @@ import (
 	"log"
 )
 
-func Connect(amqpURI string) (ch *amqp.Connection) {
-	connection, err := amqp.Dial(amqpURI)
+var (
+	ch *amqp.Channel
+)
+
+func Connect(amqpURI string) (conn *amqp.Connection) {
+	conn, err := amqp.Dial(amqpURI)
 	if err != nil {
 		log.Printf("ERROR: Could not connect to RabbitMQ server - %q", err)
 		return nil
 	} else {
-		log.Printf("Connected to RabbitMQ server - %s", amqpURI)
-		return connection
+		log.Printf("[CONEJO] Connected to RabbitMQ server - %s", amqpURI)
+		ch, err = conn.Channel()
+		if err != nil {
+			log.Printf("ERROR: Could not declare channel %q", err)
+			return nil
+		} else {
+			return conn
+		}
 	}
 }
 
-func declareExchange(ch *amqp.Channel, exchange string) error {
+func declareExchange(exchange string) error {
 	err := ch.ExchangeDeclare(
 		exchange, // name
 		"topic",  // type
@@ -29,7 +39,7 @@ func declareExchange(ch *amqp.Channel, exchange string) error {
 	return err
 }
 
-func declareQueue(ch *amqp.Channel, queue string) (q amqp.Queue, err error) {
+func declareQueue(queue string) (q amqp.Queue, err error) {
 	q, err = ch.QueueDeclare(
 		queue, // name
 		true,  // durable
@@ -39,6 +49,77 @@ func declareQueue(ch *amqp.Channel, queue string) (q amqp.Queue, err error) {
 		nil,   // arguments
 	)
 	return q, err
+}
+
+func confirmOne(confirms <-chan amqp.Confirmation) {
+	log.Printf("waiting for confirmation of one publishing")
+	if confirmed := <-confirms; confirmed.Ack {
+		log.Printf("confirmed delivery with delivery tag: %d", confirmed.DeliveryTag)
+	} else {
+		log.Printf("failed delivery of delivery tag: %d", confirmed.DeliveryTag)
+	}
+}
+
+func Publish(conn *amqp.Connection, queue, exchange string) {
+
+	var reliable = true
+	var body = "DSDH HJD SHDJ DHJKLD HASJDKL HJKLDSA"
+
+	err := declareExchange(exchange)
+	if err != nil {
+		log.Printf("ERROR: Could not declare exchange %q", err)
+	} else {
+		log.Printf("[CONEJO] Declared exchange")
+		q, err := declareQueue(queue)
+		if err != nil {
+			log.Printf("ERROR: Could not declare queue %q", err)
+		} else {
+			log.Printf("[CONEJO] Declared queue")
+			err = ch.QueueBind(
+				q.Name, // queue name
+				queue,
+				exchange, // exchange
+				false,
+				nil,
+			)
+			if err != nil {
+				log.Printf("ERROR: Could not bind queue '%s' to exchange '%s' using '%s'", queue, exchange, queue, err)
+			} else {
+
+				// Reliable publisher confirms require confirm.select support from to connection.
+				if reliable {
+					if err := ch.Confirm(false); err != nil {
+						log.Printf("ERROR: Could not confirm - %s", err)
+					}
+
+					confirms := ch.NotifyPublish(make(chan amqp.Confirmation, 1))
+
+					defer confirmOne(confirms)
+				}
+
+				if err = ch.Publish(
+					exchange, // publish to an exchange
+					q.Name,   // routing to 0 or more queues
+					false,    // mandatory
+					false,    // immediate
+					amqp.Publishing{
+						Headers:         amqp.Table{},
+						ContentType:     "text/plain",
+						ContentEncoding: "",
+						Body:            []byte(body),
+						DeliveryMode:    amqp.Transient, // 1=non-persistent, 2=persistent
+						Priority:        0,              // 0-9
+						// a bunch of application/implementation-specific fields
+					},
+				); err != nil {
+					log.Printf("ERROR: Could not publish message %s", err)
+				} else {
+					ch.Close()
+				}
+
+			}
+		}
+	}
 }
 
 func Consume(conn *amqp.Connection, exchange, queue, consumerTag string) (chan string, error) {
@@ -54,13 +135,13 @@ func Consume(conn *amqp.Connection, exchange, queue, consumerTag string) (chan s
 		log.Printf("Got Channel")
 		defer ch.Close()
 
-		err := declareExchange(ch, exchange)
+		err := declareExchange(exchange)
 		if err != nil {
 			log.Printf("ERROR: Could not declare Exchange [%s] %q", exchange, err)
 			return nil, err
 		} else {
 
-			q, err := declareQueue(ch, queue)
+			q, err := declareQueue(queue)
 			if err != nil {
 				log.Printf("ERROR: Could not declare queue [%s] %q", queue, err)
 				return nil, err
