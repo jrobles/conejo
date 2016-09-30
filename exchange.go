c696af8f6c1b1815d97ac1d1b9d5c714edf7e771
josemrobles <joserobles@robi.local>
2016-09-30 15:35:57 -0400

    new exchnge wip

--- exchange.go
+++ exchange.go
@@ -1,5 +1,19 @@
 package conejo
 
+import (
+	"github.com/streadway/amqp"
+)
+
+type exchange struct {
+	Name        string
+	Type        string
+	Durable     bool
+	AutoDeleted bool
+	Internal    bool
+	NoWait      bool
+	Arguments   amqp.Table
+}
+
 func declareExchange(exchange string) error {
 	err := ch.ExchangeDeclare(
 		exchange, // name
@@ -9,6 +23,19 @@ func declareExchange(exchange string) error {
 		false,    // internal
 		false,    // noWait
 		nil,      // arguments
+	)
+	return err
+}
+
+func _declareExchange(e exchange) error {
+	err := ch.ExchangeDeclare(
+		e.Name,        // name
+		e.Type,        // type
+		e.Durable,     // durable
+		e.AutoDeleted, // auto-deleted
+		e.Internal,    // internal
+		e.NoWait,      // noWait
+		e.Arguments,   // arguments
 	)
 	return err
 }
