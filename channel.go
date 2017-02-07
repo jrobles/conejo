19b13f2ec0b00b36caf7e3b8b961525d926cab20
josemrobles <das.haas@gmail.com>
2017-02-06 19:18:39 -0500

    Some comments, many more to go

--- channel.go
+++ channel.go
@@ -4,6 +4,10 @@ import (
 	"github.com/streadway/amqp"
 )
 
+/*
+Create a channel using an established conn [amqp.Connection]. Once the channel
+is established, return the channel [amqp.Channel].
+*/
 func createChannel(conn *amqp.Connection) (*amqp.Channel, error) {
 	channel, err := conn.Channel()
 	if err != nil {
--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"github.com/streadway/amqp"
 )
 
+/*
+Connect to the RabbitMQ Sever using the server URI [string].
+If a connections is established, return the connection [amqp.Connection].
+*/
 func Connect(amqpURI string) *amqp.Connection {
 	conn, err := amqp.Dial(amqpURI)
 	if err != nil {
